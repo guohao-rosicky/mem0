48ae806f57537a26ac700d6a40e01fe056e9d441
chengjianyun <chengjianyun@360.cn>
2025-08-04 18:29:42 +0800

    Add project_id and member_id for rest APIs URL: https://geelib.qihoo.net/geelib/repoMergeDetail?repoId=xt_hadoop/memory_layer_platform&mergeId=1

--- server/main.py
+++ server/main.py
@@ -14,49 +14,69 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 # Load environment variables
 load_dotenv()
 
+VECTOR_STORE_PROVIDER = os.environ.get("VECTOR_STORE_PROVIDER", "milvus")
+VECTOR_STORE_HOST = os.environ.get("VECTOR_STORE_HOST", "")
+VECTOR_STORE_DIMS = os.environ.get("VECTOR_STORE_DIMS", "4096")
+VECTOR_STORE_USER = os.environ.get("VECTOR_STORE_USER", "user")
+VECTOR_STORE_PASSWORD = os.environ.get("VECTORSTORE_PASSWORD", "password")
 
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "postgres")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
-POSTGRES_COLLECTION_NAME = os.environ.get("POSTGRES_COLLECTION_NAME", "memories")
+LLM_PROVIDER = os.environ.get("LLM_PROVIDER", "openai")
+LLM_MODEL = os.environ.get("LLM_MODEL", "openai")
+LLM_API_KEY = os.environ.get("LLM_API_KEY", "openai")
+LLM_BASE_URL = os.environ.get("LLM_BASE_URL", "openai")
+LLM_MAX_TOKENS = os.environ.get("LLM_MAX_TOKENS", "2000")
 
-NEO4J_URI = os.environ.get("NEO4J_URI", "bolt://neo4j:7687")
-NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME", "neo4j")
-NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD", "mem0graph")
+EMBEDDER_PROVIDER = os.environ.get("EMBEDDER_PROVIDER", "openai")
+EMBEDDER_MODEL = os.environ.get("EMBEDDER_MODEL", "")
+EMBEDDER_API_KEY = os.environ.get("EMBEDDER_API_KEY", "")
+EMBEDDER_BASE_URL = os.environ.get("EMBEDDER_BASE_URL", "")
 
-MEMGRAPH_URI = os.environ.get("MEMGRAPH_URI", "bolt://localhost:7687")
-MEMGRAPH_USERNAME = os.environ.get("MEMGRAPH_USERNAME", "memgraph")
-MEMGRAPH_PASSWORD = os.environ.get("MEMGRAPH_PASSWORD", "mem0graph")
+GRAPH_STORE_PROVIDER = os.environ.get("GRAPH_STORE_PROVIDER", "neo4j")
+GRAPH_STORE_HOST = os.environ.get("GRAPH_STORE_HOST", "")
+GRAPH_STORE_USER = os.environ.get("GRAPH_STORE_USER", "user")
+GRAPH_STORE_PASSWORD = os.environ.get("VECTORDB_PASSWORD", "password")
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-HISTORY_DB_PATH = os.environ.get("HISTORY_DB_PATH", "/app/history/history.db")
-
-DEFAULT_CONFIG = {
+SERVER_CONFIG = {
     "version": "v1.1",
     "vector_store": {
-        "provider": "pgvector",
+        "provider": VECTOR_STORE_PROVIDER,
         "config": {
-            "host": POSTGRES_HOST,
-            "port": int(POSTGRES_PORT),
-            "dbname": POSTGRES_DB,
-            "user": POSTGRES_USER,
-            "password": POSTGRES_PASSWORD,
-            "collection_name": POSTGRES_COLLECTION_NAME,
+            "url": VECTOR_STORE_HOST,
+            "embedding_model_dims": VECTOR_STORE_DIMS,
+            #"user": VECTOR_STORE_USER,
+            #"password": VECTOR_STORE_PASSWORD,
+            "collection_name": {},
+            "token": "8e4b8ca9cf2c67"
         },
     },
     "graph_store": {
-        "provider": "neo4j",
-        "config": {"url": NEO4J_URI, "username": NEO4J_USERNAME, "password": NEO4J_PASSWORD},
+        "provider": GRAPH_STORE_PROVIDER,
+        "config": {
+            "url": GRAPH_STORE_HOST, 
+            "username": GRAPH_STORE_USER, 
+            "password": GRAPH_STORE_PASSWORD
+        }
     },
-    "llm": {"provider": "openai", "config": {"api_key": OPENAI_API_KEY, "temperature": 0.2, "model": "gpt-4o"}},
-    "embedder": {"provider": "openai", "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"}},
-    "history_db_path": HISTORY_DB_PATH,
+    "llm": {
+        "provider": LLM_PROVIDER, 
+        "config": {
+            "api_key": LLM_API_KEY, 
+            "temperature": 0.2, 
+            "model": LLM_MODEL,
+            "max_tokens": int(LLM_MAX_TOKENS)
+        }
+    },
+    "embedder": {
+        "provider": EMBEDDER_PROVIDER, 
+        "config": {
+            "api_key": EMBEDDER_API_KEY, 
+            "model": EMBEDDER_MODEL
+        }
+    }
 }
 
-
-MEMORY_INSTANCE = Memory.from_config(DEFAULT_CONFIG)
+SERVER_CONFIG['llm']['config'][f'{LLM_PROVIDER}_base_url'] = LLM_BASE_URL
+SERVER_CONFIG['embedder']['config'][f'{EMBEDDER_PROVIDER}_base_url'] = EMBEDDER_BASE_URL
 
 app = FastAPI(
     title="Mem0 REST APIs",
@@ -64,37 +84,65 @@ app = FastAPI(
     version="1.0.0",
 )
 
+@app.post("/v1/register", summary="Register the project, user should call the API when add/create a project")
+def register(project_id: str, project_name: Optional[str] = None): 
+    ## TODO: generate collection name for vector store, generate space for graph store
+    return None
+
+@app.post("/v1/unregister", summary="Register the project, user should call the API when remove a project")
+def unregister(project_id: str, project_name: Optional[str] = None): 
+    ## TODO: remove project related info, WON'T clear memories the project related
+    return None
 
 class Message(BaseModel):
     role: str = Field(..., description="Role of the message (user or assistant).")
     content: str = Field(..., description="Message content.")
 
-
 class MemoryCreate(BaseModel):
+    project_id: str
+    member_id: str
     messages: List[Message] = Field(..., description="List of messages to store.")
     user_id: Optional[str] = None
     agent_id: Optional[str] = None
     run_id: Optional[str] = None
     metadata: Optional[Dict[str, Any]] = None
 
-
 class SearchRequest(BaseModel):
+    project_id: str
+    member_id: str
     query: str = Field(..., description="Search query.")
     user_id: Optional[str] = None
     run_id: Optional[str] = None
     agent_id: Optional[str] = None
     filters: Optional[Dict[str, Any]] = None
 
+######## project related APIs
+def getConfig(project_id):
+    ## TODO: read collection_name from database
+    SERVER_CONFIG["vector_store"]['config']['collection_name'] = project_id
+    logging.info(SERVER_CONFIG)
+    return SERVER_CONFIG
 
-@app.post("/configure", summary="Configure Mem0")
-def set_config(config: Dict[str, Any]):
+#@app.post("/v1/configure/update", summary="Use the API to update some user customized settings.")
+def set_config(
+    project_id: str,
+    config: Dict[str, Any]):
     """Set memory configuration."""
     global MEMORY_INSTANCE
     MEMORY_INSTANCE = Memory.from_config(config)
     return {"message": "Configuration set successfully"}
 
+######### memory and entity related APIs
+memInstances = {}
 
-@app.post("/memories", summary="Create memories")
+def getMemInstance(project_id: str):
+    if (project_id not in memInstances):
+        config = getConfig(project_id)
+        memInstances[project_id] = Memory.from_config(config)
+        
+    return memInstances[project_id]
+
+@app.post("/v1/memories", summary="Create memories")
 def add_memory(memory_create: MemoryCreate):
     """Store new memories."""
     if not any([memory_create.user_id, memory_create.agent_id, memory_create.run_id]):
@@ -102,22 +150,23 @@ def add_memory(memory_create: MemoryCreate):
 
     params = {k: v for k, v in memory_create.model_dump().items() if v is not None and k != "messages"}
     try:
-        response = MEMORY_INSTANCE.add(messages=[m.model_dump() for m in memory_create.messages], **params)
+        response = getMemInstance(memory_create.project_id).add(messages=[m.model_dump() for m in memory_create.messages], **params)
         return JSONResponse(content=response)
     except Exception as e:
         logging.exception("Error in add_memory:")  # This will log the full traceback
         raise HTTPException(status_code=500, detail=str(e))
 
-
-@app.get("/memories", summary="Get memories")
+@app.get("/v1/memories", summary="Get memories")
 def get_all_memories(
+    project_id: str,
+    member_id: str,
     user_id: Optional[str] = None,
     run_id: Optional[str] = None,
-    agent_id: Optional[str] = None,
-):
+    agent_id: Optional[str] = None):
     """Retrieve stored memories."""
     if not any([user_id, run_id, agent_id]):
         raise HTTPException(status_code=400, detail="At least one identifier is required.")
+    MEMORY_INSTANCE = getMemInstance(project_id)
     try:
         params = {
             k: v for k, v in {"user_id": user_id, "run_id": run_id, "agent_id": agent_id}.items() if v is not None
@@ -127,61 +176,74 @@ def get_all_memories(
         logging.exception("Error in get_all_memories:")
         raise HTTPException(status_code=500, detail=str(e))
 
-
-@app.get("/memories/{memory_id}", summary="Get a memory")
-def get_memory(memory_id: str):
+@app.get("/v1/memories/{memory_id}", summary="Get a memory based on it's id")
+def get_memory(
+    project_id: str,
+    member_id: str,
+    memory_id: str):
     """Retrieve a specific memory by ID."""
     try:
-        return MEMORY_INSTANCE.get(memory_id)
+        return getMemInstance(project_id).get(memory_id)
     except Exception as e:
         logging.exception("Error in get_memory:")
         raise HTTPException(status_code=500, detail=str(e))
 
-
-@app.post("/search", summary="Search memories")
+@app.post("/v1/memories/search", summary="Search memories")
 def search_memories(search_req: SearchRequest):
     """Search for memories based on a query."""
     try:
         params = {k: v for k, v in search_req.model_dump().items() if v is not None and k != "query"}
-        return MEMORY_INSTANCE.search(query=search_req.query, **params)
+        return getMemInstance(search_req.project_id).search(query=search_req.query, **params)
     except Exception as e:
         logging.exception("Error in search_memories:")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.put("/memories/{memory_id}", summary="Update a memory")
-def update_memory(memory_id: str, updated_memory: Dict[str, Any]):
+@app.put("/v1/memories/{memory_id}", summary="Update a memory")
+def update_memory(
+    project_id: str,
+    member_id: str,   
+    memory_id: str, 
+    updated_memory: Dict[str, Any]):
     """Update an existing memory."""
     try:
-        return MEMORY_INSTANCE.update(memory_id=memory_id, data=updated_memory)
+        return getMemInstance(project_id).update(memory_id=memory_id, data=updated_memory)
     except Exception as e:
         logging.exception("Error in update_memory:")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/memories/{memory_id}/history", summary="Get memory history")
-def memory_history(memory_id: str):
+@app.get("/v1/memories/{memory_id}/history", summary="Get memory history according to it's id")
+def memory_history(
+    project_id: str,
+    member_id: str,   
+    memory_id: str):
     """Retrieve memory history."""
     try:
-        return MEMORY_INSTANCE.history(memory_id=memory_id)
+        return getMemInstance(project_id).history(memory_id=memory_id)
     except Exception as e:
         logging.exception("Error in memory_history:")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.delete("/memories/{memory_id}", summary="Delete a memory")
-def delete_memory(memory_id: str):
+@app.delete("/v1/memories/{memory_id}", summary="Delete a memory according to it's id")
+def delete_memory(
+    project_id: str,
+    member_id: str,   
+    memory_id: str):
     """Delete a specific memory by ID."""
     try:
-        MEMORY_INSTANCE.delete(memory_id=memory_id)
+        getMemInstance(project_id).delete(memory_id=memory_id)
         return {"message": "Memory deleted successfully"}
     except Exception as e:
         logging.exception("Error in delete_memory:")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.delete("/memories", summary="Delete all memories")
+@app.delete("/v1/memories", summary="Delete all memories related")
 def delete_all_memories(
+    project_id: str,
+    member_id: str,   
     user_id: Optional[str] = None,
     run_id: Optional[str] = None,
     agent_id: Optional[str] = None,
@@ -193,18 +255,20 @@ def delete_all_memories(
         params = {
             k: v for k, v in {"user_id": user_id, "run_id": run_id, "agent_id": agent_id}.items() if v is not None
         }
-        MEMORY_INSTANCE.delete_all(**params)
+        getMemInstance(project_id).delete_all(**params)
         return {"message": "All relevant memories deleted"}
     except Exception as e:
         logging.exception("Error in delete_all_memories:")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/reset", summary="Reset all memories")
-def reset_memory():
+@app.post("/v1/reset", summary="Reset all memories")
+def reset_memory(
+    project_id: str,
+    member_id: str):
     """Completely reset stored memories."""
     try:
-        MEMORY_INSTANCE.reset()
+        getMemInstance(project_id).reset()
         return {"message": "All memories reset"}
     except Exception as e:
         logging.exception("Error in reset_memory:")
